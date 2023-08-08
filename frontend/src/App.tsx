7eba92eda0db8e2a238ca3426a87c9e327cf03d0
Andrew Curd <andrewc2695@gmail.com>
2023-08-08 14:35:09 -0700

    connected fe to be

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const getGame = () => {
+    return fetch('http://localhost:3000/api/v1/game_boards', {method: 'GET'}).then((res) => res.json()).then(json => json);
+  }
+
+  const game = getGame().then((game) => console.log(game));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello!
     </div>
   );
 }
